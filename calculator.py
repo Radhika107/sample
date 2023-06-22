569f6b5bb3f98268228a4e23e6498fda675c84a6
Radhika107 <radhikakancharla10@gmail.com>
2023-06-22 12:48:15 -0700

    thid id yhr doingousgrtgrjlignvdofjnkerj,tdkjg

---
+++ calculator.py
@@ -0,0 +1,22 @@
+n=int(input("Enter the number:"))
+m=int(input("Enter another number: "))
+y=input("Enter the operation: ")
+if y== "+":
+    c=n+m
+elif y=="-":
+    c=n-m
+elif y=="*":
+    c=n*m
+elif y=="/":
+    c=n/m
+print(c)
+
+
+
+
+
+
+
+
+
+
